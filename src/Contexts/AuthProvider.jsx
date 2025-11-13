1380e82fe47805e4b2c0fdb92f249589d0856483
Azim-Ul-Karim <azimulkarim.ruet@gmail.com>
2025-11-13 20:37:04 +0600

    Updated

---
+++ src/Contexts/AuthProvider.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { auth } from '../Firebase/Firebase.init';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+            .finally(() => setLoading(false));
+    }
+
+    const googleUser = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+            .finally(() => setLoading(false));
+    }
+
+    const updateUser = (updatedData) => {
+        return updateProfile(auth.currentUser, updatedData);
+    }
+
+    const forgetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const logoutUser = () => {
+        return signOut(auth);
+    }
+
+    const authInfo = {
+        user,
+        setUser,
+        loading,
+        setLoading,
+        createUser,
+        googleUser,
+        loginUser,
+        updateUser,
+        forgetPassword,
+        logoutUser
+    }
+
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
