e825b8da150513508fb2df658f04ce597641f023
Azim-Ul-Karim <azimulkarim.ruet@gmail.com>
2025-11-13 20:37:45 +0600

    Updated

---
+++ src/Contexts/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router';
+import { AuthContext } from './AuthContext';
+import Loader from '../Components/Loader';
+
+const PrivateRoute = ({ children }) => {
+
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+
+    if (loading) {
+        return <Loader />;
+    }
+
+    if (user && user?.email) {
+        return children;
+    }
+
+    return <Navigate state={location.pathname} to='/login'></Navigate>;
+};
+
+export default PrivateRoute;
